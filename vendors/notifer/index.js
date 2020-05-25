c383bf64a29b2a943186a0af2d2cdb39f812529f
chiemeke ifeanyi <chiemeke.ifeanyi@gmail.com>
2020-05-25 17:41:42 +0100

    first push

--- vendors/notifer/index.js
+++ vendors/notifer/index.js
@@ -1,8 +1,10 @@
 const notifier = require('./notifier');
 module.exports = function (Sequelize) {
 
-    Sequelize.Model.prototype.notify = function (options){
+    Sequelize.Model.prototype.notify = function (notificationObject){
+        //check if it has been instantiated
+
        return  notifier(this,options)
     }
 
-}
+};
--- vendors/notifer/notifier.js
+++ vendors/notifer/notifier.js
@@ -3,7 +3,6 @@ const transport = require('../../config/mailer')();
 module.exports = async function  notify (model,options) {
 
     try{
-        console.log('sending notification');
         let mail =  await transport.sendMail({
             from:process.env.MAIL_FROM||'no-reply',
             to:model.email,
